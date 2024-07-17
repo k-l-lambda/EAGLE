9e16197cf0264aeca8ab6f118aa83d33c5a4ddbc
k.l.lambda <k.l.la.bda@gmail.com>
2024-07-17 16:27:49 +0800

    evaluation: extracted a new script entry.py.

---
+++ eagle/evaluation/entry.py
@@ -0,0 +1,148 @@
+
+import argparse
+import json
+import os
+
+
+
+def reorg_answer_file (answer_file):
+    """Sort by question id and de-duplication"""
+    answers = {}
+    with open(answer_file, "r") as fin:
+        for l in fin:
+            qid = json.loads(l)["question_id"]
+            answers[qid] = l
+
+    qids = sorted(list(answers.keys()))
+    with open(answer_file, "w") as fout:
+        for qid in qids:
+            fout.write(answers[qid])
+
+
+def gen_entry (run_eval):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--ea-model-path",
+        type=str,
+        default="down_checkpoints/LC70B",
+        help="The path to the weights. This can be a local folder or a Hugging Face repo ID.",
+    )
+    parser.add_argument("--base-model-path", type=str, default="/home/lyh/weights/hf/llama2chat/70B/", help="1")
+    parser.add_argument("--data-path", type=str, help="input data jsonl file path")
+    parser.add_argument(
+        "--load-in-8bit", action="store_false", help="Use 8-bit quantization"
+    )
+    parser.add_argument("--model-id", type=str, default="llama38b2_40")
+    parser.add_argument(
+        "--bench-name",
+        type=str,
+        default="mt_bench",
+        help="The name of the benchmark question set.",
+    )
+    parser.add_argument(
+        "--question-begin",
+        type=int,
+        help="A debug option. The begin index of questions.",
+    )
+    parser.add_argument(
+        "--question-end", type=int, help="A debug option. The end index of questions."
+    )
+    parser.add_argument("--answer-file", type=str, help="The output answer file.")
+    parser.add_argument(
+        "--max-new-token",
+        type=int,
+        default=1024,
+        help="The maximum number of new generated tokens.",
+    )
+    parser.add_argument(
+        "--total-token",
+        type=int,
+        default=60,
+        help="number of all draft token",
+    )
+    parser.add_argument(
+        "--depth",
+        type=int,
+        default=5,
+        help="depth of draft model predition",
+    )
+    parser.add_argument(
+        "--top-k",
+        type=int,
+        default=10,
+        help="The maximum number of new generated tokens.",
+    )
+    parser.add_argument(
+        "--threshold",
+        type=float,
+        default=0.1,
+        help="The maximum number of new generated tokens.",
+    )
+    parser.add_argument(
+        "--num-choices",
+        type=int,
+        default=1,
+        help="How many completion choices to generate.",
+    )
+    parser.add_argument(
+        "--num-gpus-per-model",
+        type=int,
+        default=1,
+        help="The number of GPUs per model.",
+    )
+    parser.add_argument(
+        "--num-gpus-total", type=int, default=1, help="The total number of GPUs."
+    )
+    parser.add_argument(
+        "--max-gpu-memory",
+        type=str,
+        help="Maxmum GPU memory used for model weights per GPU.",
+    )
+
+    parser.add_argument(
+        "--temperature",
+        type=float,
+        default=0.0,
+    )
+
+    parser.add_argument(
+        "--tree-choices",
+        type=str,
+        default="mc_sim_7b_63",
+    )
+
+    args = parser.parse_args()
+
+    args.model_id = args.model_id + "-temperature-" + str(args.temperature)
+    if args.num_gpus_total // args.num_gpus_per_model > 1:
+        import ray
+
+        ray.init()
+
+    #question_file = f"{parent_dir}/data/{args.bench_name}/question-alpaca20.jsonl"
+    question_file = args.data_path
+    if args.answer_file:
+        answer_file = args.answer_file
+    else:
+        answer_file = f"{args.bench_name}/{args.model_id}-alpaca20.jsonl"
+
+    print(f"Output to {answer_file}")
+
+    run_eval(
+        args.base_model_path,
+        args.ea_model_path,
+        args.model_id,
+        question_file,
+        args.question_begin,
+        args.question_end,
+        answer_file,
+        args.max_new_token,
+        args.num_choices,
+        args.num_gpus_per_model,
+        args.num_gpus_total,
+        args.max_gpu_memory,
+        args.temperature,
+        args,
+    )
+
+    reorg_answer_file(answer_file)
